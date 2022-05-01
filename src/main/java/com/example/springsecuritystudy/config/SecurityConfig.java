9a8a710a355247ebbdda9c4abaa0667a0451dcb0
K-MHOON <azmria117@gmail.com>
2022-05-01 10:23:26 +0900

    feat : 사용자 정의 보안 기능 구현
    - Spring boot Security 의존성 추가
    - 기본 Controller, Security Config 추가

---
+++ src/main/java/com/example/springsecuritystudy/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.springsecuritystudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 인가
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+
+        // 인증
+        http.formLogin();
+    }
+}
